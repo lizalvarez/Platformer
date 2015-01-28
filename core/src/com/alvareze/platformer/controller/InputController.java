3a49fce0fc4b157b40ce5c35894e299bdb247475
lizalvarez <li.alvarez760@gamil.com>
2015-01-28 14:02:10 -0800

    i updated the map so he wont fall off to his death now

--- core/src/com/alvareze/platformer/controller/InputController.java
+++ core/src/com/alvareze/platformer/controller/InputController.java
@@ -4,24 +4,33 @@ package com.alvareze.platformer.controller;
 import com.alvareze.platformer.model.InputControl;
 import com.alvareze.platformer.model.Spritesheet;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class InputController {
+    private static ArrayList<InputControl> inputControls;
     private static Spritesheet spritesheet;
     private static InputControl right;
-    private static InputControl left;
 
 
     public static void initializeController(){
+        inputControls = new ArrayList<InputControl>();
         spritesheet = new Spritesheet("img/touch-controls.png", 80, 80);
         right = new InputControl(new Vector2(0, 0), spritesheet.spriteFrames[1], "right");
+        inputControls.add(right);
+        //creating an array for input controls
         Gdx.input.setInputProcessor(createInputAdapter());
     }
     public static void draw (Batch spriteBatch) {
         spriteBatch.begin();
-        right.draw(spriteBatch);
+        for(InputControl input : inputControls) {
+            input.draw(spriteBatch);
+
+        }
         spriteBatch.end();
     }
 
@@ -29,25 +38,47 @@ public class InputController {
         return new InputAdapter(){
             @Override
             public boolean keyDown(int keycode) {
+                if(keycode == Input.Keys.RIGHT){
+                    PlayerController.movementAction = "right";
+
+                }
                return true;
             }
 
             @Override
             public boolean keyUp(int keycode) {
+                if(keycode == Input.Keys.RIGHT){
+                    PlayerController.movementAction = "";
+                    //clear the action
+
+                }
                 return true;
             }
 
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                if(right.getBoundingBox().contains(screenX, Gdx.graphics.getHeight() - screenY)) {
-                    PlayerController.movementAction = "right";
-                 }
+                screenY = Gdx.graphics.getHeight() - screenY;
+                for(InputControl input : inputControls) {
+                    if (input.getBoundingBox().contains(screenX, screenY)) {
+                        if(input.action.equalsIgnoreCase("right")) {
+                            PlayerController.movementAction = "right";
+                        }
+                        }
+                }
                 return true;
             }
 
             @Override
             public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-                PlayerController.movementAction = "";
+                screenY = Gdx.graphics.getHeight() - screenY;
+                for(InputControl input : inputControls) {
+                    if (input.getBoundingBox().contains(screenX, screenY)) {
+                        if(input.action.equalsIgnoreCase("right")) {
+                            //checking if we let go of that button
+                            PlayerController.movementAction = "";
+                        }
+                    }
+                }
                 return true;
             }
         };
